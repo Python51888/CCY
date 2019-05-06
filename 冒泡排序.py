6fc0277dea4e9289d5ec031af7de29a001f72ef6
Python51888 <jinyuandalao@163.com>
2019-05-06 09:46:38 +0800

    自己理解的冒泡排序

---
+++ 冒泡排序.py
@@ -0,0 +1,27 @@
+#定义排序的方法
+def listSort(arr):
+    n = len(arr)
+    if n < 0:
+        return
+    #每次冒出一个数--共排的次数是总数-1
+    for i in range(n-1): #循环的次数是(总数-1)次
+        #内部比较交换的次数---是len(arr)-1-i
+        for j in range(n-1-i):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+    return arr
+    pass
+if __name__ == '__main__':
+    a = input('请输入待排序数列')
+    s = a.split() #以空格分割
+    arr = []
+    for item in s:
+        arr.append(int(item))
+
+    #开始调用方法
+    list = listSort(arr)
+    print('数据排序如下:')
+    for item in list:
+        print(item,end = ' ')
+
+
